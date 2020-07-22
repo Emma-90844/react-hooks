5b92eef9a0d21b7c3ebdf7167a4e4dba303427ca
Emma <enabojohn@gmail.com>
2020-07-22 10:27:56 +0300

    Conditionally runnig effects

--- tours/src/components/HookUseEffectOne.js
+++ tours/src/components/HookUseEffectOne.js
@@ -2,13 +2,16 @@ import React, {useState, useEffect} from 'react';
 
  function HookUseEffectOne() {
 const [count, setCount] = useState(0);
+const [name, setName] = useState(' ')
 
 //Implementing the side effect
 useEffect(() => {
+    console.log('use effect -updating document title')
     document.title = `You clicked ${count} times`
-})
+}, [count])
   return (
     <div>
+        <input type="text" value={name} onChange={e => setName(e.target.value) }/>
         <button onClick={() => setCount(count + 1)}>Click { count } Times</button>
     </div>
   );
